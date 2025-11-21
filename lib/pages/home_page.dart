a36728736d80d1f51b6694898bbdfa118b0aff0d
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-22 06:38:05 +0800

    Add HomePage with GameWidget integration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+import '../game/my_game.dart';
+import 'package:flame/game.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GameWidget(game: MyGame()),
+    );
+  }
+}
 
