cd0d541326f46612a8d93e6a0514077b7e17955a
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-22 06:38:59 +0800

    Update my_game.dart

--- lib/game/my_game.dart
+++ lib/game/my_game.dart
@@ -1,1 +1,15 @@
+import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+
+class MyGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    add(
+      TextComponent(
+        text: "Welcome to the Game!",
+        position: Vector2(50, 50),
+      ),
+    );
+  }
+}
 
