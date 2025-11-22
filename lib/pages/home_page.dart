ddbebbf3739dc32f61311b0f7c8833e0d1b70ce9
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-22 13:39:57 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import '../game/my_game.dart';
-import 'package:flame/game.dart';
+import 'character_selection_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GameWidget(game: MyGame()),
+      appBar: AppBar(title: Text("Home")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => CharacterSelectionPage()),
+            );
+          },
+          child: Text("Select Character"),
+        ),
+      ),
     );
   }
 }
-
