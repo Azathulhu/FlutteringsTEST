e7cbcad8757de911bda93bdab5e14cf0734bfbee
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-28 14:20:56 +0800

    Update player_game.dart

--- lib/flame/player_game.dart
+++ lib/flame/player_game.dart
@@ -18,20 +18,17 @@ class PlayerGame extends FlameGame {
   Future<void> onLoad() async {
     await super.onLoad();
 
-    // Background
     final bg = SpriteComponent()
       ..sprite = await loadSprite('background/$levelBackground')
       ..size = size;
     add(bg);
 
-    // Player
     final player = PlayerComponent(
       characterData: characterData,
       inputService: inputService,
     )..position = Vector2(size.x / 2, size.y / 2);
     add(player);
 
-    // Add overlay
     overlays.add('ControlsOverlay');
   }
 }
