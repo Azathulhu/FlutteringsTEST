0e821053e7b4180634eaa1eb0a061da8c393333a
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-28 14:21:50 +0800

    Update sub_level_selection_page.dart

--- lib/pages/sub_level_selection_page.dart
+++ lib/pages/sub_level_selection_page.dart
@@ -1,97 +1,61 @@
 import 'package:flutter/material.dart';
-import '../services/level_service.dart';
 import 'game_page.dart';
 
-class SubLevelSelectionPage extends StatefulWidget {
+class SubLevelSelectionPage extends StatelessWidget {
   final Map<String, dynamic> level;
-  SubLevelSelectionPage({required this.level});
+  final Map<String, dynamic> selectedCharacter;
+  final List<Map<String, dynamic>> subLevels;
 
-  @override
-  State<SubLevelSelectionPage> createState() => _SubLevelSelectionPageState();
-}
-
-class _SubLevelSelectionPageState extends State<SubLevelSelectionPage> {
-  final PageController _controller = PageController(viewportFraction: 0.6);
-  final LevelService _levelService = LevelService();
-
-  int currentPage = 0;
-  List<Map<String, dynamic>> subLevels = [];
-  bool loading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    loadSubLevels();
-  }
-
-  Future<void> loadSubLevels() async {
-    final data = await _levelService.loadSubLevels(widget.level['id']);
-    setState(() {
-      subLevels = data;
-      loading = false;
-    });
-  }
+  SubLevelSelectionPage({
+    required this.level,
+    required this.selectedCharacter,
+    required this.subLevels,
+  });
 
   @override
   Widget build(BuildContext context) {
-    if (loading) return Center(child: CircularProgressIndicator());
-
     return Scaffold(
-      appBar: AppBar(title: Text("Select Sub-Level")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 250,
-            child: PageView.builder(
-              controller: _controller,
-              itemCount: subLevels.length,
-              onPageChanged: (i) => setState(() => currentPage = i),
-              itemBuilder: (context, index) {
-                final sub = subLevels[index];
+      appBar: AppBar(title: Text('Select SubLevel')),
+      body: PageView.builder(
+        itemCount: subLevels.length,
+        controller: PageController(viewportFraction: 0.7),
+        itemBuilder: (context, index) {
+          final subLevel = subLevels[index];
+          final isUnlocked = subLevel['is_unlocked'] ?? false;
 
-                return Opacity(
-                  opacity: sub['is_unlocked'] ? 1 : 0.5,
-                  child: GestureDetector(
-                    onTap: sub['is_unlocked']
-                        ? () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => GamePage(
-                                  level: widget.level,
-                                  subLevel: sub,
-                                ),
-                              ),
-                            );
-                          }
-                        : null,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 12),
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                          color: sub['is_unlocked'] ? Colors.white : Colors.red,
-                          width: 3,
+          return GestureDetector(
+            onTap: isUnlocked
+                ? () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => GamePage(
+                          level: level,
+                          subLevel: subLevel,
+                          character: selectedCharacter, // ✅ required!
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          sub['name'],
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
+                    );
+                  }
+                : null,
+            child: Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: isUnlocked ? Colors.green : Colors.red),
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.blueGrey,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(subLevel['name'], style: TextStyle(fontSize: 24, color: Colors.white)),
+                  SizedBox(height: 10),
+                  Text(isUnlocked ? 'Unlocked' : 'Locked', style: TextStyle(color: Colors.white70)),
+                ],
+              ),
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
