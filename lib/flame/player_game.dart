2900f5f458cb89e917b0eea3b222a6fd490ee839
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-28 13:40:09 +0800

    Create player_game.dart

---
+++ lib/flame/player_game.dart
@@ -0,0 +1,41 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
+import 'player_component.dart';
+import '../services/input_service.dart';
+
+class PlayerGame extends FlameGame with HasTappables, HasDraggables, HasCollisionDetection {
+  final String levelBackground;
+  final Map<String, dynamic> characterData;
+
+  late PlayerComponent player;
+  late InputService inputService;
+
+  PlayerGame({required this.levelBackground, required this.characterData}) {
+    inputService = InputService();
+  }
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    // Background
+    add(SpriteComponent()
+      ..sprite = await loadSprite('background/$levelBackground')
+      ..size = size);
+
+    // Player
+    player = PlayerComponent(characterData: characterData, inputService: inputService)
+      ..position = Vector2(size.x / 2, 100);
+    add(player);
+
+    // Add UI buttons
+    overlays.add('ControlsOverlay');
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+}
