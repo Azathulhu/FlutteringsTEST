8508a48c165a9abf1a94e53b2626f3423cb16e4d
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-28 15:33:33 +0800

    Update game_page.dart

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,72 +1,29 @@
 import 'package:flutter/material.dart';
-import '../flame/player_game.dart';
-import 'package:flame/game.dart';
 
-class GamePage extends StatefulWidget {
+class GamePage extends StatelessWidget {
   final Map<String, dynamic> level;
   final Map<String, dynamic> subLevel;
-  final Map<String, dynamic> character;
 
-  GamePage({
-    required this.level,
-    required this.subLevel,
-    required this.character,
-  });
-
-  @override
-  State<GamePage> createState() => _GamePageState();
-}
-
-class _GamePageState extends State<GamePage> {
-  late PlayerGame game;
-
-  @override
-  void initState() {
-    super.initState();
-    game = PlayerGame(
-      backgroundImage: widget.subLevel['background_image'],
-      character: widget.character,
-    );
-  }
+  GamePage({required this.level, required this.subLevel});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          GameWidget(game: game),
-          Positioned(
-            left: 20,
-            bottom: 50,
-            child: Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    game.player.moveLeft(1 / 60); // basic step per frame
-                  },
-                  child: Icon(Icons.arrow_left),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    game.player.moveRight(1 / 60);
-                  },
-                  child: Icon(Icons.arrow_right),
-                ),
-              ],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                "assets/images/background/${level['background_image']}"),
+            fit: BoxFit.cover,
           ),
-          Positioned(
-            right: 20,
-            bottom: 50,
-            child: ElevatedButton(
-              onPressed: () {
-                game.player.jump();
-              },
-              child: Icon(Icons.arrow_upward),
-            ),
+        ),
+        child: Center(
+          child: Text(
+            "Sub-Level: ${subLevel['name']}",
+            style: TextStyle(
+                color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
           ),
-        ],
+        ),
       ),
     );
   }
