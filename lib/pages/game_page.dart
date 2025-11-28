6003150990d5f6c3a2b3169c564e881b24e9c5b2
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-28 16:35:43 +0800

    Update game_page.dart

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,50 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flame/game.dart';
-import '../game/my_platformer_game.dart';
-import '../services/character_service.dart';
-
-class GamePage extends StatefulWidget {
-  final int characterId;
-  const GamePage({super.key, required this.characterId});
-
-  @override
-  State<GamePage> createState() => _GamePageState();
-}
-
-class _GamePageState extends State<GamePage> {
-  late MyPlatformerGame game;
-  Map<String, dynamic>? characterData;
-
-  @override
-  void initState() {
-    super.initState();
-    loadCharacter();
-  }
-
-  Future<void> loadCharacter() async {
-    final data = await CharacterService().loadCharacterById(widget.characterId);
-    setState(() {
-      characterData = data;
-      game = MyPlatformerGame(characterData: data);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (characterData == null) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-
-    return Scaffold(
-      body: GameWidget(game: game),
-    );
-  }
-}
-
-
-/*import 'package:flutter/material.dart';
 
 class GamePage extends StatelessWidget {
   final Map<String, dynamic> level;
@@ -73,4 +27,4 @@ class GamePage extends StatelessWidget {
       ),
     );
   }
-}*/
+}
