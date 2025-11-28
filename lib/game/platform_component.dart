bba9dd1c7c60bf987b9addba410c0975f5514607
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-28 16:24:11 +0800

    Update platform_component.dart

--- lib/game/platform_component.dart
+++ lib/game/platform_component.dart
@@ -1,26 +1,12 @@
 import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
 
 class PlatformComponent extends PositionComponent {
-  PlatformComponent({
-    required Vector2 position,
-    required Vector2 size,
-  }) {
-    this.position = position;
-    this.size = size;
-    anchor = Anchor.topLeft;
-  }
-
-  @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-  }
+  PlatformComponent({required Vector2 position, required Vector2 size})
+      : super(position: position, size: size);
 
   @override
   void render(Canvas canvas) {
-    super.render(canvas);
-    final rect = size.toRect();
-    final paint = Paint()..color = Colors.brown;
-    canvas.drawRect(rect, paint);
+    final paint = Paint()..color = const Color(0xFF888888);
+    canvas.drawRect(size.toRect(), paint);
   }
 }
