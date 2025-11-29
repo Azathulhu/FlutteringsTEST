7a88ba9179121679a68fc433eae5cc9906530f7d
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-11-29 08:23:34 +0800

    Create platform.dart

---
+++ lib/game/platform.dart
@@ -0,0 +1,13 @@
+class Platform {
+  double x;
+  double y;
+  double width;
+  double height;
+
+  Platform({
+    required this.x,
+    required this.y,
+    required this.width,
+    this.height = 20,
+  });
+}
