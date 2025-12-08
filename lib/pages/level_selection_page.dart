82fcf4705911a782cbed9a21e000f9286802494f
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-12-08 15:33:17 +0800

    Update level_selection_page.dart

--- lib/pages/level_selection_page.dart
+++ lib/pages/level_selection_page.dart
@@ -1,122 +1,89 @@
+// lib/pages/level_selection_page.dart
 import 'package:flutter/material.dart';
 import '../services/level_service.dart';
 import 'sub_level_selection_page.dart';
 
 class LevelSelectionPage extends StatefulWidget {
-  final List<Map<String, dynamic>>? preloadedLevels;
-
-  LevelSelectionPage({this.preloadedLevels});
-
   @override
   State<LevelSelectionPage> createState() => _LevelSelectionPageState();
 }
 
 class _LevelSelectionPageState extends State<LevelSelectionPage> {
-  final PageController _controller = PageController(viewportFraction: 0.7);
   final LevelService _levelService = LevelService();
-
-  int currentPage = 0;
   List<Map<String, dynamic>> levels = [];
   bool loading = true;
 
   @override
   void initState() {
     super.initState();
-    if (widget.preloadedLevels != null) {
-      levels = widget.preloadedLevels!;
-      loading = false;
-    } else {
-      loadLevels();
-    }
+    loadLevels();
   }
 
   Future<void> loadLevels() async {
     setState(() => loading = true);
     final data = await _levelService.loadLevels();
-    if (data.isEmpty) {
-      print("Levels list is empty!");
-    }
     setState(() {
       levels = data;
       loading = false;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     if (loading) return Scaffold(body: Center(child: CircularProgressIndicator()));
 
     return Scaffold(
-      appBar: AppBar(title: Text("Select Biome")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 300,
-            child: PageView.builder(
-              controller: _controller,
-              itemCount: levels.length,
-              onPageChanged: (i) => setState(() => currentPage = i),
-              itemBuilder: (context, index) {
-                final level = levels[index];
-                final unlocked = level['is_unlocked'] == true;
+      appBar: AppBar(title: Text("Select Level")),
+      body: PageView.builder(
+        controller: PageController(viewportFraction: 0.7),
+        itemCount: levels.length,
+        itemBuilder: (context, index) {
+          final lvl = levels[index];
+          final unlocked = lvl['is_unlocked'] == true;
 
-                return GestureDetector(
-                  onTap: unlocked
-                      ? () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => SubLevelSelectionPage(
-                                level: level,
-                              ),
-                            ),
-                          );
-                        }
-                      : null, // disabled if locked
-                  child: Opacity(
-                    opacity: unlocked ? 1.0 : 0.5,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 12),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                            color: unlocked ? Colors.white : Colors.grey,
-                            width: 3),
-                        image: DecorationImage(
-                          image: AssetImage(
-                              "assets/images/background/${level['background_image']}"),
-                          fit: BoxFit.cover,
-                        ),
+          return GestureDetector(
+            onTap: unlocked
+                ? () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => SubLevelSelectionPage(level: lvl),
                       ),
-                      child: Center(
-                        child: Text(
-                          level['name'],
-                          style: TextStyle(
-                            fontSize: 28,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            shadows: [
-                              Shadow(
-                                  color: Colors.black,
-                                  offset: Offset(2, 2),
-                                  blurRadius: 4),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                    );
+                  }
+                : null,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 60),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: unlocked ? Colors.white : Colors.grey, width: 3),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/background/${lvl['background_image']}"),
+                  fit: BoxFit.cover,
+                  colorFilter: unlocked
+                      ? null
+                      : ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  lvl['name'],
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    shadows: [Shadow(color: Colors.black, offset: Offset(2, 2), blurRadius: 4)],
                   ),
-                );
-              },
+                ),
+              ),
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
 }
+
 
 
 
