bcebee562a155e597fab9f17e3d5ef97152e7f59
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-12-13 20:25:47 +0800

    Update sub_level_selection_page.dart

--- lib/pages/sub_level_selection_page.dart
+++ lib/pages/sub_level_selection_page.dart
@@ -41,13 +41,11 @@ class _SubLevelSelectionPageState extends State<SubLevelSelectionPage> {
           level: widget.level,
           subLevel: subLevel,
           onLevelComplete: () async {
-            // Reload sub-levels immediately after completing one
             await _loadSubLevels();
           },
         ),
       ),
     );
-    // Reload sub-levels in case player unlocked another one
     _loadSubLevels();
   }
 
@@ -105,106 +103,3 @@ class _SubLevelSelectionPageState extends State<SubLevelSelectionPage> {
     );
   }
 }
-
-
-
-
-
-/*import 'package:flutter/material.dart';
-import '../services/level_service.dart';
-import 'game_page.dart';
-
-class SubLevelSelectionPage extends StatefulWidget {
-  final Map<String, dynamic> level;
-  SubLevelSelectionPage({required this.level});
-
-  @override
-  State<SubLevelSelectionPage> createState() => _SubLevelSelectionPageState();
-}
-
-class _SubLevelSelectionPageState extends State<SubLevelSelectionPage> {
-  final PageController _controller = PageController(viewportFraction: 0.6);
-  final LevelService _levelService = LevelService();
-
-  int currentPage = 0;
-  List<Map<String, dynamic>> subLevels = [];
-  bool loading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    loadSubLevels();
-  }
-
-  Future<void> loadSubLevels() async {
-    final data = await _levelService.loadSubLevels(widget.level['id']);
-    setState(() {
-      subLevels = data;
-      loading = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (loading) return Center(child: CircularProgressIndicator());
-
-    return Scaffold(
-      appBar: AppBar(title: Text("Select Sub-Level")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 250,
-            child: PageView.builder(
-              controller: _controller,
-              itemCount: subLevels.length,
-              onPageChanged: (i) => setState(() => currentPage = i),
-              itemBuilder: (context, index) {
-                final sub = subLevels[index];
-
-                return Opacity(
-                  opacity: sub['is_unlocked'] ? 1 : 0.5,
-                  child: GestureDetector(
-                    onTap: sub['is_unlocked']
-                        ? () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => GamePage(
-                                  level: widget.level,
-                                  subLevel: sub,
-                                ),
-                              ),
-                            );
-                          }
-                        : null,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 12),
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                          color: sub['is_unlocked'] ? Colors.white : Colors.red,
-                          width: 3,
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          sub['name'],
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}*/
